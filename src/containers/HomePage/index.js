e90e6884f721ba96ff6695b08edf4477783b3ad8
henz <henz.jb@gmail.com>
2018-10-27 20:04:46 +0900

    refactor dispatch using actions

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -4,6 +4,7 @@ import ImgWrapper from './ImgWrapper';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 import { connect }from 'react-redux';
+import { incrementNum } from 'actions';
 
 class Index extends Component {
   render() {
@@ -29,14 +30,13 @@ const mapStateToProps = (state) => {
   return {
     outputButtonTxt: state.num
   }
-}
+};
+
 const mapDispatchToProps = (dispatch) => {
   return {
-    onClick: () => {
-      dispatch({type: 'INCREMENT_NUM'})
-    }
+    onClick: () => dispatch(incrementNum()),
   }
-}
+};
 
 export default connect(
   mapStateToProps,
