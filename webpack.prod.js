1346bff7f923c0defa7c4da5b7501977d2b0addc
henz <henz.jb@gmail.com>
2018-10-27 17:12:19 +0900

    implement clean-webpack-plugin when rebuilding

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CleanWebpackPlugin = require('clean-webpack-plugin'); //installed via npm
+
+let pathsToClean = [
+  'dist',
+];
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin(pathsToClean),
+  ]
+
 });
