c4fcd23d13d965c53841bd64c10bebeb5063caa6
henz <henz.jb@gmail.com>
2018-10-28 08:24:15 +0900

    wrap App with ConnectedRouter component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './containers/App/index';
-import "babel-polyfill";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { reducers } from './reducers';
+import React from 'react'
+import { render } from 'react-dom'
+import App from './containers/App/index'
+import "babel-polyfill"
+import { Provider } from 'react-redux'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { reducers } from './reducers'
+import { createBrowserHistory } from 'history'
+import { connectRouter, routerMiddleware, ConnectedRouter }
+  from 'connected-react-router'
 
 const rootEl = document.getElementById('app');
+const history = createBrowserHistory();
+const initialState = {};
 
-let store = createStore(reducers);
+const store = createStore(
+  connectRouter(history)(reducers), // new root reducer with router state
+  initialState,
+  compose(
+    applyMiddleware(
+      routerMiddleware(history), // for dispatching history actions
+      // ... other middlewares ...
+    ),
+  ),
+);
 
 render(
   <Provider store={store}>
+    <ConnectedRouter history={history}>
       <App />
+    </ConnectedRouter>
   </Provider>,
   rootEl
 );
