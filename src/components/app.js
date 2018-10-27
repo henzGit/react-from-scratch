e8e1bed5298416b9d10cd69981e02699c85eb6c7
henz <henz.jb@gmail.com>
2018-10-27 19:06:46 +0900

    button click is connected to react component defaultProps

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Icon from '../public/img/logo.svg';
 import ImgWrapper from './ImgWrapper';
 import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   render() {
@@ -9,12 +10,22 @@ class App extends Component {
       <div>
         <h1>My React App</h1>
         <ImgWrapper src={Icon}/>
-        <Button variant="contained" color="primary">
+        <Button variant="contained"
+                onClick={this.props.onClick}
+                color="primary">
           Hello World
         </Button>
       </div>
     )
   }
 }
+
+App.propTypes = {
+  onClick: PropTypes.func.isRequired
+};
+
+App.defaultProps = {
+  onClick: () => console.log('default click action')
+};
 
 export default App;
