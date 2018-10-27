3da0314a4ea54ca75a025aa6383ef74e452d20a1
henz <henz.jb@gmail.com>
2018-10-27 20:16:22 +0900

    connect to store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App/index';
 import "babel-polyfill";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducers } from './reducers';
 
 const rootEl = document.getElementById('app');
 
-render(<App />, rootEl);
+let store = createStore(reducers);
+
+render(
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  rootEl
+);
 
 if (module.hot) {
   module.hot.accept();
