df7e3fac7c2e558087b16d9407e3f9e0c684dec7
henz <henz.jb@gmail.com>
2018-10-28 11:05:53 +0900

    add GOT_STUFF and GOT_NO_STUFF constants

--- src/containers/HomePage/constants.js
+++ src/containers/HomePage/constants.js
@@ -1,3 +1,5 @@
 export const INCREMENT_NUM = 'app/App/INCREMENT_NUM';
 export const GET_STUFF = 'app/App/GET_STUFF';
+export const GOT_STUFF = 'app/App/GOT_STUFF';
+export const GOT_NO_STUFF = 'app/App/GOT_NO_STUFF';
 
