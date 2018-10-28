17815ee437ee99a5a8fab08d7074979c73337f3c
henz <henz.jb@gmail.com>
2018-10-28 12:26:27 +0900

    get data from selector in HomePage saga

--- src/containers/HomePage/saga.js
+++ src/containers/HomePage/saga.js
@@ -1,11 +1,14 @@
-import { takeLatest, put, call } from 'redux-saga/effects'
+import { takeLatest, put, call, select } from 'redux-saga/effects'
 import { GET_STUFF} from './constants'
 import {gotNoStuff, gotStuff} from './actions'
 import api from './api'
+import { selectCurrentUser } from './selectors';
+
 
 function* getStuff() {
   try {
-    console.log('trying to get stuff: ');
+    const currentUser = yield select(selectCurrentUser);
+    console.log('trying to get stuff for : ' + currentUser);
     const data = yield call(api.getStuff);
     yield put(gotStuff(data));
   } catch (error) {
