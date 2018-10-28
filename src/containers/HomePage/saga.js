a332dc4ef40439f3dd5c117036c49e52b64c1bbe
henz <henz.jb@gmail.com>
2018-10-28 11:21:40 +0900

    configure HomePage saga

--- src/containers/HomePage/saga.js
+++ src/containers/HomePage/saga.js
@@ -1,59 +1,19 @@
-/**
- * Authenticate user to an API end point
- */
+import { takeLatest, put, call } from 'redux-saga/effects'
+import { GET_STUFF} from './constants'
+import {gotNoStuff, gotStuff} from './actions'
+import api from './api'
 
-import { call, put, select, takeLatest } from 'redux-saga/effects';
-import { AUTHENTICATE } from './constants';
-import {
-  authenticationError,
-  authenticationSuccess,
-} from './actions';
-
-import request from 'utils/request';
-import { makeSelectUsername, makeSelectPassword } from './selectors';
-import { push } from 'react-router-redux';
-import { updateUserData}  from '../App/actions';
-import { Cookies } from 'react-cookie';
-
-/**
- * Auth API request/response handler
- */
-export function* callAuthApi() {
-  const username = yield select(makeSelectUsername());
-  const password = yield select(makeSelectPassword());
-  const requestURL =
-    `https://api.github.com/users/${username}/repos?type=all&sort=updated`;
-
+function* getStuff() {
   try {
-    // Call our request helper (see 'utils/request')
-    const response = yield call(request, requestURL);
-    yield put(authenticationSuccess());
-
-    const userData = {
-      isAuthenticated: true,
-      userId: 1,
-      currentUserName: 'great Henz'
-    };
-    yield put(updateUserData(userData));
-
-    const cookies = new Cookies();
-    const cookieValues = {
-      userId: 1,
-      authId: 'rwerwrerawrw',
-    };
-    cookies.set('siteCookie', cookieValues);
-    yield put(push('/app'));
-  } catch (err) {
-    yield put(authenticationError(err));
+    console.log('trying to get stuff: ');
+    const data = yield call(api.getStuff);
+    console.log(data);
+    yield put(gotStuff(data));
+  } catch (error) {
+    console.log('saga failed: ', error);
+    yield put(gotNoStuff(error))
   }
 }
-
-/**
- * Root saga manages watcher lifecycle
- */
-export default function* authenticate() {
-  // By using `takeLatest` only the result of the latest API call is applied.
-  // It returns task descriptor (just like fork) so we can continue execution
-  // It will be cancelled automatically on component unmount
-  yield takeLatest(AUTHENTICATE, callAuthApi);
+export function* homePageSagas() {
+  yield takeLatest(GET_STUFF, getStuff);
 }
